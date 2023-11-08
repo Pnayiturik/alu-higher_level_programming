726c178fdc85d4ab9baf19575b9738a196c4c3ae
Pnayiturik <p.nayiturik@alustudent.com>
2023-11-08 15:27:57 +0200

    jnnsd

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    def remove_letter(string, letter):
-        new_string = ''
-        for char in string:
-            if char != letter:
-                new_string += char
-        return new_string
+    new_string = ''
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_string += char
+    return new_string
 
-    letter = "C"
-    return remove_letter(my_string, letter)
+
+print(no_c("Best School"))
+print(no_c("Chicago"))
+print(no_c("C is fun!"))
